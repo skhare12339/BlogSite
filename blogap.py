4fda7c79e1c77698f7e2c1382f1060b1c4ff8313
skhare12339 <70477796+skhare12339@users.noreply.github.com>
2021-10-16 14:25:42 +0530

    Add files via upload

---
+++ blogap.py
@@ -0,0 +1,24 @@
+import tornado.web
+import tornado.ioloop
+
+
+class mainpage(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello Noobs")
+
+class htmlpage(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+
+if __name__ == "__main__":
+    app=tornado.web.Application([
+        (r"/",mainpage),
+        (r"/html",htmlpage)
+
+    ])
+
+
+app.listen(8881)
+print("I am listening to 8881")
+tornado.ioloop.IOLoop.current().start()
